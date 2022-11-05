9d52383017d1a0d7d860975a9a01abc2354f7858
Adrian <luisadrian@gmail.com>
2022-11-05 09:39:53 -0700

    Initial Commit

---
+++ Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Luis Garcia on 05/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
